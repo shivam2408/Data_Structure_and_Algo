b82a62a14d580e8d5a04c166ae40c3e36ff66473
Shivam Kumar Mishra <shivammishra2408@gmail.com>
2020-11-13 20:39:35 +0530

    graphs

--- Array Questions/02_01Normal_find_min_max.cpp
+++ Array Questions/02_01Normal_find_min_max.cpp
@@ -1,30 +1,30 @@
-//#include<iostream>
-//using namespace std;
-//
-//int main() {
-//
-//	int n;
-//	cin >> n;
-//	int a[1024] = { 0 };
-//	for (int i = 0; i < n; i++) {
-//		cin >> a[i];
-//	}
-//
-//	int max = INT_MIN;
-//	int min = INT_MAX;
-//
-//	for (int i = 0; i < n; i++) {
-//
-//		if (a[i] > max) {
-//			max = a[i];
-//		}
-//		if (a[i] < min) {
-//			min = a[i];
-//		}
-//
-//	}
-//	cout << "Minimum " << min << endl;
-//	cout << "Maximum " << max << endl;
-//	
-//	
-//}
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	int a[1024] = { 0 };
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	int max = INT_MIN;
+	int min = INT_MAX;
+
+	for (int i = 0; i < n; i++) {
+
+		if (a[i] > max) {
+			max = a[i];
+		}
+		if (a[i] < min) {
+			min = a[i];
+		}
+
+	}
+	cout << "Minimum " << min << endl;
+	cout << "Maximum " << max << endl;
+	
+	
+}
--- Array Questions/04_Sort012.cpp
+++ Array Questions/04_Sort012.cpp
@@ -1,23 +1,23 @@
-//#include<iostream>
-//using namespace std;
-//
-//int main() {
-//	
-//	int a[10] = {1,2,1,0,2,1,1,0,2,0};
-//	int zeros=0, ones=0, twos=0;
-//	for (int i = 0; i < 10; i++) {
-//		if (a[i] == 0) {
-//			++zeros;
-//		}
-//		else if (a[i] == 1) {
-//			++ones;
-//		}
-//		else if (a[i] == 2) {
-//			++twos;
-//		}
-//	}
-//	cout << " " << zeros << " " << ones << " "<<twos;
-//
-//
-//
-//}
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	int a[10] = {1,2,1,0,2,1,1,0,2,0};
+	int zeros=0, ones=0, twos=0;
+	for (int i = 0; i < 10; i++) {
+		if (a[i] == 0) {
+			++zeros;
+		}
+		else if (a[i] == 1) {
+			++ones;
+		}
+		else if (a[i] == 2) {
+			++twos;
+		}
+	}
+	cout << " " << zeros << " " << ones << " "<<twos;
+
+
+
+}
